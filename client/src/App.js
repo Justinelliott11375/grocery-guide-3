f76c6af86dea1ebb62d3c6d26da6b095fb701a28
Justin Elliott <Justinelliott11375@gmail.com>
2018-11-17 04:03:22 -0600

    grocery item add/delete functioning

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavBar';
+import GroceryList from './components/GroceryList';
+import ItemModal from './components/itemModal';
+import { Container} from 'reactstrap';
+import { Provider } from 'react-redux';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <Container>
+            <ItemModal />
+            <GroceryList />
+          </Container>
+        </div>
+      </Provider>
     );
   }
 }
